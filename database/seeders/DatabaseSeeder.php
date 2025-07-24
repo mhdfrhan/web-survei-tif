252d96665d179b07583b561f10db1ab7a7d49de3
mhdfrhan <mhdfarhan5148@gmail.com>
2025-07-24 10:50:59 +0700

    selesai

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,11 +15,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        // User::factory()->create([
-        //     'name' => 'Admin',
-        //     'email' => 'adminsurveitif@gmail.com',
-        //     'password' => bcrypt('adminsurveitif!@#)(*'),
-        // ]);
+        User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'adminsurveitif@gmail.com',
+            'password' => bcrypt('adminsurveitif!@#)(*'),
+        ]);
 
         $this->call([
             SurveyResponseDosenSeeder::class,
